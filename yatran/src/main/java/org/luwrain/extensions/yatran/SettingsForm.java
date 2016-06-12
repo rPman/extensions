f05009a4d326f25cec144de17b250e778899ba04
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-06-12 17:22:42 +0700

    yatran:New control panel interface support

--- yatran/src/main/java/org/luwrain/extensions/yatran/SettingsForm.java
+++ yatran/src/main/java/org/luwrain/extensions/yatran/SettingsForm.java
@@ -43,6 +43,28 @@ class SettingsForm extends FormArea implements SectionArea
 	addEdit("key", strings.formAccessKey(), settings.getKey(""));
     }
 
+    @Override public boolean onKeyboardEvent(KeyboardEvent event)
+    {
+	NullCheck.notNull(event, "event");
+	if (controlPanel.onKeyboardEvent(event))
+	    return true;
+	return super.onKeyboardEvent(event);
+    }
+
+    @Override public boolean onEnvironmentEvent(EnvironmentEvent event)
+    {
+	NullCheck.notNull(event, "event");
+	if (controlPanel.onEnvironmentEvent(event))
+	    return true;
+	return super.onEnvironmentEvent(event);
+    }
+
+    @Override public boolean saveSectionData()
+    {
+	return true;
+    }
+
+
     static SettingsForm create(ControlPanel controlPanel)
     {
 	NullCheck.notNull(controlPanel , "controlPanel");
